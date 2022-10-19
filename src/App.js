b11369b3d87e019e74ff417a55db153a8b99da47
stovecbr <pc-018@st-ove.co.jp>
2022-10-19 14:39:10 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';  //useStateの読み込み
+import './styles.css';
+// import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+export const App=  () =>  {
+  const [todoText, setTodoText] = useState("");
+  
+  const [incompleteTodos, setIncompleteTodos] = useState([1,2]);
+  const [completeTodos, setCompleteTodos] = useState([3])
+  
+  const onChangeTodoText = (event) => setTodoText(event.target.value );
+
+  const onClickAdd = () => {
+    if(todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  // 元のコード
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index,1);
+    setIncompleteTodos(newTodos);
+  };
+
+  // // onClickDeliteに追加  未完了と完了が同時に削除されてしまう
+  // const onClickDelete = (index) => {
+  //   const newTodos = [...incompleteTodos];
+  //   const newTodosComp = [...completeTodos]; //追加
+  //   newTodos.splice(index,1);
+  //   newTodosComp.splice(index,1); //追加
+  //   setIncompleteTodos(newTodos);
+    // setCompleteTodos(newTodosComp); //追加
+  // };
+
+  
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index,1);
+    setIncompleteTodos(newIncompleteTodos);
+
+    const newCompleteTodos = [...completeTodos,incompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);  
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index,1);
+    setCompleteTodos(newCompleteTodos);
+
+    const newIncompleteTodos = [...incompleteTodos,completeTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+  }
+// onClickDeleteComplete追加
+  const onClickDeleteComplete = (index) => {
+    const newTodos = [...completeTodos];
+    newTodos.splice(index,1);
+    setCompleteTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className='input-area'>
+        <input
+         placeholder='TODOを入力'
+         value={todoText}
+         onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
+      </div>
+      <div className='incomplete-area'>
+        <p className='title'>未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo,index) => {
+            return(
+              <div key={index}  className='list-row' >
+                <li>{todo}</li>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
+              </div>
+            );
+          })}        
+        </ul>
+      </div>
+      <div className='complete-area'>
+      {/* onClickDeleteComplete追加 */}
+        <p className='title'>完了のTODO</p>
+          <ul>
+            {completeTodos.map((todo,index) => {
+              return(
+                <div key={index}  className='list-row' >
+                <li>{todo}</li>
+                <button onClick={() => onClickBack(index)}>戻る</button>
+                <button onClick={() => onClickDeleteComplete(index)}>削除</button>   {/* 追加 */}
+                </div>
+              );
+            })}       
 
-export default App;
+        {/* onClickDeleteのみの場合 */}
+        {/* <p className='title'>完了のTODO</p>
+          <ul>
+            {completeTodos.map((todo,index) => {
+              return(
+                <div key={todo} className='list-row'>
+                <li>{todo}</li>
+                <button onClick={() => onClickBack(index)}>戻る</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>  {/* 削除ボタン追加 */}
+                {/* </div> */}
+              {/* );
+            })}        */} 
+
+        {/* 元のコード    */}
+        {/* <p className='title'>完了のTODO</p>
+          <ul>
+            {completeTodos.map((todo,index) => {
+              return(
+                <div key={todo} className='list-row'>
+                <li>{todo}</li>
+                <button onClick={() => onClickBack(index)}>戻る</button>
+                </div>
+              );
+            })}             */}
+
+          </ul>
+      </div>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+import {App} from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +10,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// // bundle.js:228 Uncaught TypeError: react_dom_client__WEBPACK_IMPORTED_MODULE_1__.render is not a function
